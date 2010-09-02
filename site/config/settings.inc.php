4cbb349fba7159fc8d3baa6a222fc1b932491aec
flagos <flagospub@gmail.com>
2010-09-02 21:52:14 +0200

    contenu de prestashop

---
+++ site/config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_TYPE_', 'MySQL');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'prestashop');
+define('_DB_PASSWD_', 'mimibio');
+define('_DB_PREFIX_', 'ps_');
+define('__PS_BASE_URI__', '/prestashop/');
+define('_THEME_NAME_', 'prestashop');
+define('_COOKIE_KEY_', 'nk0FmmFyL6K9ZX7P853jImWh5XnKEmvyJ5VG2vceXA8RbXdMWo8hAqrC');
+define('_COOKIE_IV_', 'pIrEu5RG');
+define('_PS_CREATION_DATE_', '2010-08-31');
+define('_PS_VERSION_', '1.3.1.1');
+?>
---
+++ site/tools/smarty/compile/%%A5^A51^A5148094%%header.tpl.php
@@ -0,0 +1,8 @@
+<?php /* Smarty version 2.6.20, created on 2010-09-02 19:43:27
+         compiled from /var/www/prestashop/modules/blocksearch/header.tpl */ ?>
+<!-- Block search module HEADER -->
+<link rel="stylesheet" type="text/css" href="<?php echo $this->_tpl_vars['content_dir']; ?>
+css/jquery.autocomplete.css" />
+<script type="text/javascript" src="<?php echo $this->_tpl_vars['content_dir']; ?>
+js/jquery/jquery.autocomplete.js"></script>
+<!-- Block search module HEADER -->
