62349a135599586ea5aea90d2ed368d23781e700
flagos <flagospub@gmail.com>
2010-10-02 14:35:53 +0200

    Patch memcache for categories widget

--- site/modules/blockcategories/blockcategories.php
+++ site/modules/blockcategories/blockcategories.php
@@ -2,52 +2,52 @@
 
 class BlockCategories extends Module
 {
-	function __construct()
-	{
-		$this->name = 'blockcategories';
-		$this->tab = 'Blocks';
-		$this->version = 1.0;
+  function __construct()
+  {
+    $this->name = 'blockcategories';
+    $this->tab = 'Blocks';
+    $this->version = 1.0;
 
-		parent::__construct();
+    parent::__construct();
 
-		$this->displayName = $this->l('Categories block');
-		$this->description = $this->l('Adds a block featuring product categories');
-	}
+    $this->displayName = $this->l('Categories block');
+    $this->description = $this->l('Adds a block featuring product categories');
+  }
 
-	function install()
-	{
-		if (parent::install() == false
-			OR $this->registerHook('leftColumn') == false
-			OR Configuration::updateValue('BLOCK_CATEG_MAX_DEPTH', 3) == false
-			OR Configuration::updateValue('BLOCK_CATEG_DHTML', 1) == false)
-			return false;
-		return true;
-	}
+  function install()
+  {
+    if (parent::install() == false
+	OR $this->registerHook('leftColumn') == false
+	OR Configuration::updateValue('BLOCK_CATEG_MAX_DEPTH', 3) == false
+	OR Configuration::updateValue('BLOCK_CATEG_DHTML', 1) == false)
+      return false;
+    return true;
+  }
 
-	public function getContent()
-	{
-		$output = '<h2>'.$this->displayName.'</h2>';
-		if (Tools::isSubmit('submitBlockCategories'))
-		{
-			$maxDepth = intval(Tools::getValue('maxDepth'));
-			$dhtml = Tools::getValue('dhtml');
-			if ($maxDepth < 0)
-				$output .= '<div class="alert error">'.$this->l('Maximum depth: Invalid number.').'</div>';
-			elseif ($dhtml != 0 AND $dhtml != 1)
-				$output .= '<div class="alert error">'.$this->l('Dynamic HTML: Invalid choice.').'</div>';
-			else
-			{
-				Configuration::updateValue('BLOCK_CATEG_MAX_DEPTH', intval($maxDepth));
-				Configuration::updateValue('BLOCK_CATEG_DHTML', intval($dhtml));
-				$output .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="'.$this->l('Confirmation').'" />'.$this->l('Settings updated').'</div>';
-			}
-		}
-		return $output.$this->displayForm();
-	}
+  public function getContent()
+  {
+    $output = '<h2>'.$this->displayName.'</h2>';
+    if (Tools::isSubmit('submitBlockCategories'))
+      {
+	$maxDepth = intval(Tools::getValue('maxDepth'));
+	$dhtml = Tools::getValue('dhtml');
+	if ($maxDepth < 0)
+	  $output .= '<div class="alert error">'.$this->l('Maximum depth: Invalid number.').'</div>';
+	elseif ($dhtml != 0 AND $dhtml != 1)
+	  $output .= '<div class="alert error">'.$this->l('Dynamic HTML: Invalid choice.').'</div>';
+	else
+	  {
+	    Configuration::updateValue('BLOCK_CATEG_MAX_DEPTH', intval($maxDepth));
+	    Configuration::updateValue('BLOCK_CATEG_DHTML', intval($dhtml));
+	    $output .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="'.$this->l('Confirmation').'" />'.$this->l('Settings updated').'</div>';
+	  }
+      }
+    return $output.$this->displayForm();
+  }
 
-	public function displayForm()
-	{
-		return '
+  public function displayForm()
+  {
+    return '
 		<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
 			<fieldset>
 				<legend><img src="'.$this->_path.'logo.gif" alt="" title="" />'.$this->l('Settings').'</legend>
@@ -68,102 +68,108 @@ class BlockCategories extends Module
 				<center><input type="submit" name="submitBlockCategories" value="'.$this->l('Save').'" class="button" /></center>
 			</fieldset>
 		</form>';
-	}
+  }
 
-	function getTree($resultParents, $resultIds, $maxDepth, $id_category = 1, $currentDepth = 0)
-	{
-		global $link;
+  function getTree($resultParents, $resultIds, $maxDepth, $id_category = 1, $currentDepth = 0)
+  {
+    global $link;
 		
-		$children = array();
-		if (isset($resultParents[$id_category]) AND sizeof($resultParents[$id_category]) AND ($maxDepth == 0 OR $currentDepth < $maxDepth))
-			foreach ($resultParents[$id_category] as $subcat)
-				$children[] = $this->getTree($resultParents, $resultIds, $maxDepth, $subcat['id_category'], $currentDepth + 1);
-		if (!isset($resultIds[$id_category]))
-			return false;
-		return array('id' => $id_category, 'link' => $link->getCategoryLink($id_category, $resultIds[$id_category]['link_rewrite']),
-					 'name' => Category::hideCategoryPosition($resultIds[$id_category]['name']), 'desc'=> $resultIds[$id_category]['description'],
-					 'children' => $children);
-	}
+    $children = array();
+    if (isset($resultParents[$id_category]) AND sizeof($resultParents[$id_category]) AND ($maxDepth == 0 OR $currentDepth < $maxDepth))
+      foreach ($resultParents[$id_category] as $subcat)
+	$children[] = $this->getTree($resultParents, $resultIds, $maxDepth, $subcat['id_category'], $currentDepth + 1);
+    if (!isset($resultIds[$id_category]))
+      return false;
+    return array('id' => $id_category, 'link' => $link->getCategoryLink($id_category, $resultIds[$id_category]['link_rewrite']),
+		 'name' => Category::hideCategoryPosition($resultIds[$id_category]['name']), 'desc'=> $resultIds[$id_category]['description'],
+		 'children' => $children);
+  }
 
-	function hookLeftColumn($params)
-	{
-		global $smarty, $cookie, $memcache;
+  function hookLeftColumn($params)
+  {
+    global $smarty, $cookie, $memcache;
 
-		/*  ONLY FOR THEME OLDER THAN v1.0 */
-		global $link;
-		$smarty->assign(array(
-			'categories' => Category::getHomeCategories(intval($params['cookie']->id_lang), true),
-			'link' => $link
-		));
-		/* ELSE */
+    /*  ONLY FOR THEME OLDER THAN v1.0 */
+    global $link;
+    // $smarty->assign(array(
+    // 			  'categories' => Category::getHomeCategories(intval($params['cookie']->id_lang), true),
+    // 			  'link' => $link
+    // 			  ));
+    /* ELSE */
 
-               $smarty_backup = $memcache->get('categories_subblock'); 
-
-               if ( $smarty_backup == FALSE)
-               {
-		$id_customer = intval($params['cookie']->id_customer);
-		$maxdepth = Configuration::get('BLOCK_CATEG_MAX_DEPTH');
+    $var_backup = $memcache->get('categories_subblock'); 
+    if ( $var_backup == FALSE)
+      {
+	$id_customer = intval($params['cookie']->id_customer);
+	$maxdepth = Configuration::get('BLOCK_CATEG_MAX_DEPTH');
 		
-		if (!$result = Db::getInstance()->ExecuteS('
+	if (!$result = Db::getInstance()->ExecuteS('
 		SELECT DISTINCT c.*, cl.*
 		FROM `'._DB_PREFIX_.'category` c 
 		LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.intval($params['cookie']->id_lang).')
 		LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = c.`id_category`)
 		WHERE 1'
-		.(intval($maxdepth) != 0 ? ' AND `level_depth` <= '.intval($maxdepth) : '').'
+						   .(intval($maxdepth) != 0 ? ' AND `level_depth` <= '.intval($maxdepth) : '').'
 		AND (c.`active` = 1 OR c.`id_category`= 1)
 		AND cg.`id_group` '.(!$cookie->id_customer ?  '= 1' : 'IN (SELECT id_group FROM '._DB_PREFIX_.'customer_group WHERE id_customer = '.intval($cookie->id_customer).')').'
 		ORDER BY `level_depth` ASC, cl.`name` ASC'))
-			return;
-		$resultParents = array();
-		$resultIds = array();
+	  return;
+	$resultParents = array();
+	$resultIds = array();
 
-		foreach ($result as $row)
-		{
-			$row['name'] = Category::hideCategoryPosition($row['name']);
-			$resultParents[$row['id_parent']][] = $row;
-			$resultIds[$row['id_category']] = $row;
-		}
-		$blockCategTree = $this->getTree($resultParents, $resultIds, Configuration::get('BLOCK_CATEG_MAX_DEPTH'));
-		$isDhtml = (Configuration::get('BLOCK_CATEG_DHTML') == 1 ? true : false);
-		$smarty->assign('isDhtml', $isDhtml);
-		$smarty->assign('blockCategTree', $blockCategTree);
+	foreach ($result as $row)
+	  {
+	    $row['name'] = Category::hideCategoryPosition($row['name']);
+	    $resultParents[$row['id_parent']][] = $row;
+	    $resultIds[$row['id_category']] = $row;
+	  }
+	$blockCategTree = $this->getTree($resultParents, $resultIds, Configuration::get('BLOCK_CATEG_MAX_DEPTH'));
+	$isDhtml = (Configuration::get('BLOCK_CATEG_DHTML') == 1 ? true : false);
+	
+	$memcache->set ('categories_subblock', 
+			Array('blockCategTree' => $blockCategTree,
+			      'isDhtml'        => $isDhtml),
+			MEMCACHE_COMPRESSED, 3600 );  /* Keep it a day */
 
-		}else{
-		$memcache->set ('categories_subblock', $smarty ,MEMCACHE_COMPRESSED, 3600 );  /* Keep it a day */
-		}
-
-		if (isset($_GET['id_category']))
-		{
-			$cookie->last_visited_category = intval($_GET['id_category']);
-			$smarty->assign('currentCategoryId', intval($_GET['id_category']));	
-		}
-		if (isset($_GET['id_product']))
-		{			
-			if (!isset($cookie->last_visited_category) OR !Product::idIsOnCategoryId(intval($_GET['id_product']), array('0' => array('id_category' => $cookie->last_visited_category))))
-			{
-				$product = new Product(intval($_GET['id_product']));
-				if (isset($product) AND Validate::isLoadedObject($product))
-					$cookie->last_visited_category = intval($product->id_category_default);
-			}
-			$smarty->assign('currentCategoryId', intval($cookie->last_visited_category));
-		}	
+      }else{
+      $blockCategTree = $var_backup['blockCategTree'];
+      $isDhtml = $var_backup['isDhtml'];
+    }
+	       
+    $smarty->assign('isDhtml', $isDhtml);
+    $smarty->assign('blockCategTree', $blockCategTree);
+	
+    if (isset($_GET['id_category']))
+      {
+	$cookie->last_visited_category = intval($_GET['id_category']);
+	$smarty->assign('currentCategoryId', intval($_GET['id_category']));	
+      }
+    if (isset($_GET['id_product']))
+      {			
+	if (!isset($cookie->last_visited_category) OR !Product::idIsOnCategoryId(intval($_GET['id_product']), array('0' => array('id_category' => $cookie->last_visited_category))))
+	  {
+	    $product = new Product(intval($_GET['id_product']));
+	    if (isset($product) AND Validate::isLoadedObject($product))
+	      $cookie->last_visited_category = intval($product->id_category_default);
+	  }
+	$smarty->assign('currentCategoryId', intval($cookie->last_visited_category));
+      }	
 
 		
-		if (file_exists(_PS_THEME_DIR_.'modules/blockcategories/blockcategories.tpl'))
-			$smarty->assign('branche_tpl_path', _PS_THEME_DIR_.'modules/blockcategories/category-tree-branch.tpl');
-		else
-			$smarty->assign('branche_tpl_path', _PS_MODULE_DIR_.'blockcategories/category-tree-branch.tpl');
+    if (file_exists(_PS_THEME_DIR_.'modules/blockcategories/blockcategories.tpl'))
+      $smarty->assign('branche_tpl_path', _PS_THEME_DIR_.'modules/blockcategories/category-tree-branch.tpl');
+    else
+      $smarty->assign('branche_tpl_path', _PS_MODULE_DIR_.'blockcategories/category-tree-branch.tpl');
 
-		/* /ONLY FOR THEME OLDER THAN v1.0 */
+    /* /ONLY FOR THEME OLDER THAN v1.0 */
 		
-		return $this->display(__FILE__, 'blockcategories.tpl');
-	}
+    return $this->display(__FILE__, 'blockcategories.tpl');
+  }
 
-	function hookRightColumn($params)
-	{
-		return $this->hookLeftColumn($params);
-	}
+  function hookRightColumn($params)
+  {
+    return $this->hookLeftColumn($params);
+  }
 }
 
 ?>
--- site/modules/blockinfos/blockinfos.php
+++ site/modules/blockinfos/blockinfos.php
@@ -2,54 +2,54 @@
 
 class BlockInfos extends Module
 {
-	private $_html;
+  private $_html;
 
-	public function __construct()
-	{
-		$this->name = 'blockinfos';
-		$this->tab = 'Blocks';
-		$this->version = 1.1;
+  public function __construct()
+  {
+    $this->name = 'blockinfos';
+    $this->tab = 'Blocks';
+    $this->version = 1.1;
 
-		parent::__construct();
+    parent::__construct();
 
-		$this->displayName = $this->l('Info block');
-		$this->description = $this->l('Adds a block with several information links');
-	}
+    $this->displayName = $this->l('Info block');
+    $this->description = $this->l('Adds a block with several information links');
+  }
 
-	public function install()
-	{
-		if (!parent::install() OR !$this->registerHook('leftColumn'))
-			return false;
-		Db::getInstance()->Execute('
+  public function install()
+  {
+    if (!parent::install() OR !$this->registerHook('leftColumn'))
+      return false;
+    Db::getInstance()->Execute('
 			INSERT INTO `'._DB_PREFIX_.'block_cms`(`id_block`, `id_cms`)
 			VALUES
 				('.intval($this->id).', 1),
 				('.intval($this->id).', 2),
 				('.intval($this->id).', 3),
 				('.intval($this->id).', 4)');
-		return true;
-	}
+    return true;
+  }
 
-	public function uninstall()
-	{
-		Db::getInstance()->Execute('DELETE FROM `'._DB_PREFIX_.'block_cms` WHERE `id_block` ='.intval($this->id));
-		parent::uninstall();
-	}
+  public function uninstall()
+  {
+    Db::getInstance()->Execute('DELETE FROM `'._DB_PREFIX_.'block_cms` WHERE `id_block` ='.intval($this->id));
+    parent::uninstall();
+  }
 
-	public function getContent()
-	{
-		if(isset($_POST['btnSubmit']))
-			$this->_postProcess();
-		$this-> _displayForm();
-		return $this->_html;
-	}
+  public function getContent()
+  {
+    if(isset($_POST['btnSubmit']))
+      $this->_postProcess();
+    $this-> _displayForm();
+    return $this->_html;
+  }
 
-	private function _displayForm()
-	{
-		global $cookie;
+  private function _displayForm()
+  {
+    global $cookie;
 
-		$this->_html .=
-		'<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+    $this->_html .=
+      '<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
 			<fieldset>
 			<legend>'.$this->l('Selected files displayed').'</legend>
 					<span>'.$this->l('Please check files that will be displayed in this module').'.</span><br /><br />
@@ -62,54 +62,55 @@ class BlockInfos extends Module
 								</tr>
 							</thead>
 							<tbody>';
-		$cms = CMS::listCms(intval($cookie->id_lang));
-		foreach($cms AS $row)
-			$this->_html .='
+    $cms = CMS::listCms(intval($cookie->id_lang));
+    foreach($cms AS $row)
+      $this->_html .='
 								<tr><td><input type="checkbox" class="noborder" value="'.intval($row['id_cms']).'" name="categoryBox[]" '.((CMS::isInBlock(intval($row['id_cms']), intval($this->id))) ? 'checked="checked"' : '').'></td><td>'.intval($row['id_cms']).'</td><td>'.$row['meta_title'].'</td></tr>';
-		$this->_html .='
+    $this->_html .='
 							</tbody>
 						</table>
 						<br />
 						<input type="submit" name="btnSubmit" class="button" value="'.$this->l('Update').'">
 			</fieldset>
 		</form>';
-	}
+  }
 
-	private function _postProcess()
-	{
-		if (Tools::isSubmit('btnSubmit'))
-		{
-			$categoryBox = Tools::getValue('categoryBox');
-			if ($categoryBox AND is_array($categoryBox) AND count($categoryBox))
-			{
-				foreach ($categoryBox AS $row)
-					$cms[] = intval($row);
-				if (CMS::updateCmsToBlock($cms, intval($this->id)))
-					$this->_html .= '<div class="conf confirm">'.$this->l('Cms Updated').'</div>';
-			}
-			else
-			{
-				Db::getInstance()->Execute('
+  private function _postProcess()
+  {
+    if (Tools::isSubmit('btnSubmit'))
+      {
+	$categoryBox = Tools::getValue('categoryBox');
+	if ($categoryBox AND is_array($categoryBox) AND count($categoryBox))
+	  {
+	    foreach ($categoryBox AS $row)
+	      $cms[] = intval($row);
+	    if (CMS::updateCmsToBlock($cms, intval($this->id)))
+	      $this->_html .= '<div class="conf confirm">'.$this->l('Cms Updated').'</div>';
+	  }
+	else
+	  {
+	    Db::getInstance()->Execute('
 				DELETE FROM `'._DB_PREFIX_.'block_cms`
 				WHERE `id_block` ='.intval($this->id));
-			}
-		}
-	}
+	  }
+      }
+  }
 
-	function hookLeftColumn($params)
-	{
-		global $smarty, $cookie;
+  function hookLeftColumn($params)
+  {
+    global $smarty, $cookie;
 
-		$cms = CMS::listCms($cookie->id_lang, $this->id);
-		$id_cms = array();
-		foreach($cms AS $row)
-			$id_cms[] = intval($row['id_cms']);
-		$smarty->assign('cmslinks', $id_cms ? CMS::getLinks($cookie->id_lang, $id_cms) : array());
-		return $this->display(__FILE__, 'blockinfos.tpl');
-	}
+    
+    $cms = CMS::listCms($cookie->id_lang, $this->id);
+    $id_cms = array();
+    foreach($cms AS $row)
+      $id_cms[] = intval($row['id_cms']);
+    $smarty->assign('cmslinks', $id_cms ? CMS::getLinks($cookie->id_lang, $id_cms) : array());
+    return $this->display(__FILE__, 'blockinfos.tpl');
+  }
 
-	function hookRightColumn($params)
-	{
-		return $this->hookLeftColumn($params);
-	}
+  function hookRightColumn($params)
+  {
+    return $this->hookLeftColumn($params);
+  }
 }
