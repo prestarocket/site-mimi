7ddd6dbe9e7e76e042e699943b6bc69e7abc352f
flagos <flagospub@gmail.com>
2011-02-24 13:27:39 +0100

    ajout bandeaux

--- site/tools/smarty/compile/%%01^019^019CFDC0%%index.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2010-09-26 20:52:23
-         compiled from /srv/http/themes/prestashop/index.tpl */ ?>
-<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
--- site/tools/smarty/compile/%%08^081^08168FBF%%index.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2010-09-20 22:47:49
-         compiled from /home/flagos/Projects/lilibio/themes/prestashop/index.tpl */ ?>
-<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
